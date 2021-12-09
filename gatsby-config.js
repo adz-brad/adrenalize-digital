a1608b9e846731f33224e69d89798fe173f3f195
Brad S <dev@neuralsmart.ca>
2021-12-09 09:04:25 -0700

    update offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,14 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+         workboxConfig: {
+            globPatterns: ['*.html']
+         }
+      }
+   }
     {
       resolve: `gatsby-source-filesystem`,
       options: {
