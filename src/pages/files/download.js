ab7aa8e463c9e4d7c9b0e45e039e38f6101bfbaf
Brad S <brad@adrenalizedigital.ca>
2022-06-03 12:43:09 -0600

    add dl link

--- src/pages/files/download.js
+++ src/pages/files/download.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Downloads = () => {
     return(
         <div>
-            <a className="border-2 p-4 font-bold text-lg" download>
+            <a className="border-2 p-4 font-bold text-lg" href="https://github.com/brad-adrenalize/adrenalize-digital/blob/main/files/jax-guitar-images.zip" download>
                 Download Jax Guitar Images
             </a>
         </div>
