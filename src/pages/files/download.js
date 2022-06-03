dcbac47aa79ab78a86bc9807a010d13cbd721226
Brad S <brad@adrenalizedigital.ca>
2022-06-03 12:41:06 -0600

    add files directory

---
+++ src/pages/files/download.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Downloads = () => {
+    return(
+        <div>
+            <a className="border-2 p-4 font-bold text-lg" download>
+                Download Jax Guitar Images
+            </a>
+        </div>
+    )
+}
+
+export default Downloads
