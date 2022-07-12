de5897b0ed13af459972d059b9a87a551565179f
Brad S <brad@adrenalizedigital.ca>
2022-07-12 19:53:09 -0400

    add Jax Snax DL links

--- src/pages/files/download.js
+++ src/pages/files/download.js
@@ -6,6 +6,12 @@ const Downloads = () => {
             <a className="border-2 m-4 p-4 font-bold text-lg" href="https://github.com/brad-adrenalize/adrenalize-digital/raw/main/files/jax-guitar-images.zip" download>
                 Download Jax Guitar Images
             </a>
+            <a className="border-2 m-4 p-4 font-bold text-lg" href="https://github.com/brad-adrenalize/adrenalize-digital/raw/main/files/jaxsnax185x6.svg" download>
+                Download Jax Snax 18.5" x 6" SVG
+            </a>
+            <a className="border-2 m-4 p-4 font-bold text-lg" href="https://github.com/brad-adrenalize/adrenalize-digital/raw/main/files/jaxsnax16x6.svg" download>
+            Download Jax Snax 16" x 6" SVG
+            </a>
         </div>
     )
 }
