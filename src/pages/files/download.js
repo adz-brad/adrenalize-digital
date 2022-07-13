85fb57a145502eaeddce05a883bfb0cc68236c5e
Brad S <brad@adrenalizedigital.ca>
2022-07-12 20:09:59 -0400

    modify jax snax DL links to zip file

--- src/pages/files/download.js
+++ src/pages/files/download.js
@@ -2,15 +2,12 @@ import React from 'react'
 
 const Downloads = () => {
     return(
-        <div>
+        <div lcassName="mt-8">
             <a className="border-2 m-4 p-4 font-bold text-lg" href="https://github.com/brad-adrenalize/adrenalize-digital/raw/main/files/jax-guitar-images.zip" download>
                 Download Jax Guitar Images
             </a>
-            <a className="border-2 m-4 p-4 font-bold text-lg" href="https://github.com/brad-adrenalize/adrenalize-digital/raw/main/files/jaxsnax185x6.svg" download>
-                Download Jax Snax 18.5" x 6" SVG
-            </a>
-            <a className="border-2 m-4 p-4 font-bold text-lg" href="https://github.com/brad-adrenalize/adrenalize-digital/raw/main/files/jaxsnax16x6.svg" download>
-            Download Jax Snax 16" x 6" SVG
+            <a className="border-2 m-4 p-4 font-bold text-lg" href="https://github.com/brad-adrenalize/adrenalize-digital/raw/main/files/jaxSnax-SVG.zip" download>
+                Download Jax Snax SVG Files
             </a>
         </div>
     )
